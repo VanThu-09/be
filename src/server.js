324e116ece3ba59cc3e2f8c5c8990af9a45f8ade
VanThu-09 <nguyenvanthu16092004@gmail.com>
2025-04-16 00:20:45 +0700

    mysql

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 const hostname = process.env.HOSTNAME || 'localhost'
 const configViewEngine = require('./config/viewEngine')
 const webRoute = require('./routes/web')
-
+const mysql = require('mysql2')
 
 app.use(morgan('combined'))
 app.use('/', webRoute)
@@ -15,3 +15,18 @@ configViewEngine(app)
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port: 3307,
+    password: '123456',
+    database: 'hoidanit',
+});
+connection.query(
+    'select * from Users u;',
+    function (err, results, fields) {
+        console.log(">>>>results = ", results);
+        console.log(">>>>fields = ", fields);
+    }
+)
